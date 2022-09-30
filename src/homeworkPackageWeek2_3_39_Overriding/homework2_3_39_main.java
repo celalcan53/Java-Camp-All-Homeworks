67c80db59ce8d8e8bf8037414c2883853ce528ba
cca <codenamecca@hotmail.com>
2022-09-30 19:25:13 +0300

    homeworkPackageWeek2_3_39_Overriding file added.

--- src/homeworkPackageWeek2_3_39_Overriding/homework2_3_39_main.java
+++ src/homeworkPackageWeek2_3_39_Overriding/homework2_3_39_main.java
@@ -16,7 +16,7 @@ public class homework2_3_39_main {
 			// Örneğin BaseKrediManger'da kullandığımız operasyonu (return tutar*1.18;),
 			// OgrenciKrediManager'da farklı bir şekilde kullanarak (return tutar * 1.10;)
 			// ezdik. İkisi de belirttiğimiz şekilde çalıştı.
-///////////////////////////////////////////////////////////////////////////////////
+			///////////////////////////////////////////////////////////////////////////////////
 			// Ezilmesini(Override edilmesini) engellemek istiyorsak, final keywordu
 			// kullanmalıyız. Örnek Kullanım;
 			// public final double hesapla(double tutar) {
